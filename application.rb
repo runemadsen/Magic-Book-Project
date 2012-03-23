a870b132fe68355abee43de14fd84641d5630e3e
Daniel Shiffman <daniel@shiffman.net>
2012-03-23 18:16:28 -0400

    playing around with dimensions and CSS for PDF

--- application.rb
+++ application.rb
@@ -18,6 +18,16 @@ class Application < Sinatra::Base
   get '/create_pdf' do
     
     args = []
+    # Margins 0.75 inches bottom, left and right, not sure about top yet b/c of header
+    args << { :option => "--margin-bottom", :value => 10 }
+    args << { :option => "--margin-left", :value => 19.05 }
+    args << { :option => "--margin-right", :value => 19.05 }
+    
+    # Page dimensions, 7.5x9.25 inches
+    args << { :option => "--page-height", :value => 234.95 }
+    args << { :option => "--page-width", :value => 190.5 }
+    
+    
     args << { :option => "--header-html", :value => "templates/print/views/header.html"}
     args << { :option => "--header-spacing", :value => 10} # make space between header and content
     args << { :option => "--margin-top", :value => 30} # the header spacing moves the header up, so push it down again
