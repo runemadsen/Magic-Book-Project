1d270628ca6b2a5f221bcbc32739aade9ec91616
Rune Madsen <rune@runemadsen.com>
2012-07-30 17:46:47 -0400

    creating chapters in individual HTML files now working. Links not working

--- application.rb
+++ application.rb
@@ -13,12 +13,19 @@ class Application < Sinatra::Base
   get '/create_html/*' do
     asciidoc_file = "/" + params[:splat][0]
     output_folder = File.join(File.dirname(asciidoc_file), "noc_html")
-    template_folder = File.join(output_folder, "views")
-    debug_file = File.join(output_folder, "index.xml")
-    output_file = File.join(output_folder, "index.html")
     
-    @document = AsciiDoc::AsciiDocument.new(asciidoc_file, { :debug_xml_to_file => debug_file })
-    @document.render(:html, :template => template_folder, :output => output_file)
+    @document = AsciiDoc::AsciiDocument.new(asciidoc_file, {:debug_xml_to_file => File.join(output_folder, "index.xml") })
+    @document.render(:html, :template => File.join(output_folder, "views"), :output => File.join(output_folder, "index.html"))
+    
+    "Done!"
+  end
+  
+  get '/create_html_chapters/*' do
+    asciidoc_file = "/" + params[:splat][0]
+    output_folder = File.join(File.dirname(asciidoc_file), "noc_html")
+    
+    @document = AsciiDoc::AsciiDocument.new(asciidoc_file, {:debug_xml_to_file => File.join(output_folder, "index.xml") })
+    @document.render(:html_chapters, :template => File.join(output_folder, "views"), :output => File.join(output_folder, "index.html"))
     
     "Done!"
   end
@@ -27,17 +34,15 @@ class Application < Sinatra::Base
     
     asciidoc_file = "/" + params[:splat][0]
     output_folder = File.join(File.dirname(asciidoc_file), "noc_pdf")
-    template_folder = File.join(output_folder, "views")
-    debug_file = File.join(output_folder, "index.xml")
 
     startTime = Time.now
     puts "///////// Starting Render: #{startTime}"
 
     # first render html
-    @document = AsciiDoc::AsciiDocument.new(asciidoc_file, { :debug_xml_to_file => debug_file })
+    @document = AsciiDoc::AsciiDocument.new(asciidoc_file, { :debug_xml_to_file => File.join(output_folder, "index.xml") })
     puts "Parsed Asciidoc after: #{Time.now-startTime} seconds"
 
-    @document.render(:html, :template => template_folder, :output => File.join(output_folder, "index.html"))
+    @document.render(:html, :template => File.join(output_folder, "views"), :output => File.join(output_folder, "index.html"))
     puts "Rendered HTML after: #{Time.now-startTime} seconds"
 
     @document.render(:pdf, :html_file => File.join(output_folder, "index.html"), :output => File.join(output_folder, "index.pdf"))
